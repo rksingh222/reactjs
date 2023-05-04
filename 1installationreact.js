a77bf8554b024b9279c705ed0329765773f911c2
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-05-04 21:14:14 +1000

    Update 1installationreact.js

--- 1installationreact.js
+++ 1installationreact.js
@@ -1,3 +1,23 @@
 Install visual studio code
 Install prettier
 Install material icon theme
+
+Install node for mac or windows as required
+
+create react app search in google
+npx create-react-app my-app
+cd my-app
+npm start
+
+you can run the app using initially
+localhost:3000
+if you go to Sources then localhost:3000/ static/js 0.chunck.js
+
+Inside App.js it is JSX
+
+also you need to understand here two things
+
+You can just go to My mac laptop
+In Documents/React JavaScript/initial Reactjs code 
+use this code for all your react app 
+
