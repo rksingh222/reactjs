b5c213491e99030cd511d9161b90f83f4bd250df
rksingh222 <62049275+rksingh222@users.noreply.github.com>
2023-06-15 12:52:00 +1000

    Create addmealsinfirebase.js

---
+++ addmealsinfirebase.js
@@ -0,0 +1,9 @@
+In the firebase 
+real time database click on +
+first add meals next to meals add + again
+then name = m1  no value add + again beside it
+name = name and value = sushi
+add again + after m1 beside it
+name = description and value = finest fish and veggies
+name = price and value = 16.25
+you continue doing this 
